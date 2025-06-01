0a48c5f07ce0d73ccc9f308218907691f2fd902b
Arunmohankml <arunmohankrl@gmail.com>
2025-06-01 11:12:14 +0530

    Update example.lua

--- example_folder/example.lua
+++ example_folder/example.lua
@@ -1,1 +1,6 @@
-
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		DisplayTextWithLiteralString(0.5, 0.8, "STRING", "This text will be transilated")-- here "STRING" wont be transilated as its a blacklisted word
+	end
+end)
